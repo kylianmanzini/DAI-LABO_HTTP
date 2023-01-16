bae6ea37f238f40d1e4236c2346871caecc74b69
Kylian <42773865+kylianmanzini@users.noreply.github.com>
2023-01-16 12:20:14 +0100

    async api fetch done

--- express-image/src/index.js
+++ express-image/src/index.js
@@ -6,42 +6,23 @@ var app = express();
 
 var port = 3000;
 
-app.get('/', function (req,res){
-    res.send(getZoo());
-    console.log("Access to /");
-});
-
 app.get('/api', function (req,res){
-    res.send(getZoo());
-    console.log("Access to /api");
+    res.send(getAnimal());
+    console.log("Access to /api from " + req.ip + " " + req.hostname);
 });
 
 app.listen(port, function (){
     console.log("Http requests port " + port);
 });
 
-function getZoo(){
-    var zoo = [];
-    for (i = 0; i < 5; ++i){
-        zoo[i] = {
-            population: chance.integer({min: 1, max: 5}),
-            species : chance.animal(),
-            overseer: chance.name(),
-            cleaningDay: chance.weekday(),
-            feedingDay: chance.weekday(),
-            animals: []
-        }
-        for (j = 0; j < zoo[i].population; ++j){
-            zoo[i].animals[j] = {
-                name : chance.last(),
-                gender : chance.gender(),
-                birthday :chance.birthday(
-                    { string: true, american: false, year:chance.year({min: 1980, max:2022}) }
-                )
-            }
-        }
-
-    }
-
-    return zoo;
+function getAnimal(){
+    return {
+        species: chance.animal(),
+        overseer: chance.name(),
+        name: chance.last(),
+        gender: chance.gender(),
+        birthday: chance.birthday(
+            {string: true, american: false, year: chance.year({min: 1980, max: 2022})}
+        )
+    };
 }
