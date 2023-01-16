c062bef00fd90c5228f24ef25e56d797a50b30ad
Kylian <42773865+kylianmanzini@users.noreply.github.com>
2023-01-16 09:27:49 +0100

    minor fixes

--- sh/build.sh
+++ sh/build.sh
@@ -3,7 +3,7 @@ echo "Building image"
 echo $1
 if [ $1 == "dai/apache" ]; then
   docker build -t $1 -f "apache-image/Dockerfile" "apache-image/"
-elif [ $1 == "dai/express" ]; then
+elif [ $1 == "dai/api" ]; then
   docker build -t $1 -f "express-image/Dockerfile" "express-image/"
 fi
 
