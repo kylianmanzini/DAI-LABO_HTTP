7574b3040c8c9e1473dbf5e8ee3866009a4b6abe
Kylian <42773865+kylianmanzini@users.noreply.github.com>
2023-01-18 19:59:44 +0100

    fix start

--- sh/start.sh
+++ sh/start.sh
@@ -2,9 +2,9 @@ echo "#####"
 echo "Starting container"
 
 if [ $1 == "dai/apache" ]; then
-  docker run -p 9000:80 -t "dai/apache"  $1
+  docker run -p 9000:80 "dai/apache"
 elif [ $1 == "dai/api" ]; then
-  docker run -p 9001:3000 -t "dai/api" $1
+  docker run -p 9001:3000 "dai/api"
 else
   echo "image is not correct"
 fi
